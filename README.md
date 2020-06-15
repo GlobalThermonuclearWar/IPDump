f0016bc87800c902d6c18aa8653915e7215495e7
fputs <adambruce0108@gmail.com>
2020-06-15 17:50:46 +0100

    Reupload

---
+++ README.md
@@ -0,0 +1,230 @@
+# IPDump
+## A python utility for quickly generating reports for IP Addresses, Websites and domains.
+
+Current features:
+- Geolocation Information
+- SSL Certificate Fetching
+- WHOIS Information
+- Portscanning
+- Import-able
+
+## Requirements
+To use IPDump, you will need Python 3.
+
+Additionally, you will need to install some additional dependencies:
+```
+pip3 install -r requirements.txt
+```
+
+## Usage
+```
+./ipdump.py -h
+usage: ipdump.py [-h] [-l] [-c] [-a] [-p] [-i] [-s] [-w] [-n WORKERS]
+                 [-r RANGE]
+                 host
+
+positional arguments:
+  host                  The hostname/IP Address, URL or Domain of the target
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -l, --no-logging      Disable logging
+  -c, --no-color        Disable colored logging
+  -a, --all             Run all tools on the given target
+  -p, --port-scan       Enable portscanning on the target
+  -i, --ip-info         Fetch information from api-ip.com (contains
+                        geographical info)
+  -s, --ssl-cert        Retrieves the SSL Certificate of the host
+  -w, --whois           Fetch whois information from arin.net (contains domain
+                        ownership info)
+  -n WORKERS, --workers WORKERS
+                        Number of workers for portscanning
+  -r RANGE, --range RANGE
+                        Range of ports to scan formatted as START-END
+
+```
+
+## Examples: 
+
+### Getting IP Geolocation Information
+```
+./ipdump.py 91.7.125.52 -i 
+[+] WARNING: I am not liable for any damage (including criminal charges) which may arise from use of this software. For more information see the LICENSE file included with this software.
+
+[+] Requesting information from http://ip-api.com/json/
+[+] Response from http://ip-api.com/json/:
+status       : success
+continent    : Europe
+continentCode: EU
+country      : Germany
+countryCode  : DE
+region       : NW
+regionName   : North Rhine-Westphalia
+city         : Kempen
+district     : 
+zip          : 47906
+lat          : 51.3643
+lon          : 6.4186
+timezone     : Europe/Berlin
+currency     : EUR
+isp          : Deutsche Telekom AG
+org          : Deutsche Telekom AG
+as           : AS3320 Deutsche Telekom AG
+asname       : DTAG
+reverse      : p5B077D34.dip0.t-ipconnect.de
+mobile       : False
+proxy        : False
+query        : 91.7.125.52
+[+] Report for 91.7.125.52 completed
+
+```
+
+### Getting SSL Certificates
+```
+./ipdump.py google.com -s
+[+] WARNING: I am not liable for any damage (including criminal charges) which may arise from use of this software. For more information see the LICENSE file included with this software.
+
+[+] Certificate: 
+subject             : ((('countryName', 'US'),), (('stateOrProvinceName', 'California'),), (('localityName', 'Mountain View'),), (('organizationName', 'Google LLC'),), (('commonName', '*.google.com'),))
+issuer              : ((('countryName', 'US'),), (('organizationName', 'Google Trust Services'),), (('commonName', 'GTS CA 1O1'),))
+version             : 3
+serialNumber        : C5D40BA32A0CF5570800000000287A46
+notBefore           : Jan  7 15:47:12 2020 GMT
+notAfter            : Mar 31 15:47:12 2020 GMT
+subjectAltName      : (('DNS', '*.google.com'), ('DNS', '*.android.com'), ('DNS', '*.appengine.google.com'), ('DNS', '*.cloud.google.com'), ('DNS', '*.crowdsource.google.com'), ('DNS', '*.g.co'), ('DNS', '*.gcp.gvt2.com'), ('DNS', '*.gcpcdn.gvt1.com'), ('DNS', '*.ggpht.cn'), ('DNS', '*.gkecnapps.cn'), ('DNS', '*.google-analytics.com'), ('DNS', '*.google.ca'), ('DNS', '*.google.cl'), ('DNS', '*.google.co.in'), ('DNS', '*.google.co.jp'), ('DNS', '*.google.co.uk'), ('DNS', '*.google.com.ar'), ('DNS', '*.google.com.au'), ('DNS', '*.google.com.br'), ('DNS', '*.google.com.co'), ('DNS', '*.google.com.mx'), ('DNS', '*.google.com.tr'), ('DNS', '*.google.com.vn'), ('DNS', '*.google.de'), ('DNS', '*.google.es'), ('DNS', '*.google.fr'), ('DNS', '*.google.hu'), ('DNS', '*.google.it'), ('DNS', '*.google.nl'), ('DNS', '*.google.pl'), ('DNS', '*.google.pt'), ('DNS', '*.googleadapis.com'), ('DNS', '*.googleapis.cn'), ('DNS', '*.googlecnapps.cn'), ('DNS', '*.googlecommerce.com'), ('DNS', '*.googlevideo.com'), ('DNS', '*.gstatic.cn'), ('DNS', '*.gstatic.com'), ('DNS', '*.gstaticcnapps.cn'), ('DNS', '*.gvt1.com'), ('DNS', '*.gvt2.com'), ('DNS', '*.metric.gstatic.com'), ('DNS', '*.urchin.com'), ('DNS', '*.url.google.com'), ('DNS', '*.wear.gkecnapps.cn'), ('DNS', '*.youtube-nocookie.com'), ('DNS', '*.youtube.com'), ('DNS', '*.youtubeeducation.com'), ('DNS', '*.youtubekids.com'), ('DNS', '*.yt.be'), ('DNS', '*.ytimg.com'), ('DNS', 'android.clients.google.com'), ('DNS', 'android.com'), ('DNS', 'developer.android.google.cn'), ('DNS', 'developers.android.google.cn'), ('DNS', 'g.co'), ('DNS', 'ggpht.cn'), ('DNS', 'gkecnapps.cn'), ('DNS', 'goo.gl'), ('DNS', 'google-analytics.com'), ('DNS', 'google.com'), ('DNS', 'googlecnapps.cn'), ('DNS', 'googlecommerce.com'), ('DNS', 'source.android.google.cn'), ('DNS', 'urchin.com'), ('DNS', 'www.goo.gl'), ('DNS', 'youtu.be'), ('DNS', 'youtube.com'), ('DNS', 'youtubeeducation.com'), ('DNS', 'youtubekids.com'), ('DNS', 'yt.be'))
+OCSP                : ('http://ocsp.pki.goog/gts1o1',)
+caIssuers           : ('http://pki.goog/gsr2/GTS1O1.crt',)
+crlDistributionPoints: ('http://crl.pki.goog/GTS1O1.crl',)
+[+] Report for google.com completed
+```
+
+
+### Getting Website WHOIS Information
+```
+./ipdump.py github.com -w
+[+] WARNING: I am not liable for any damage (including criminal charges) which may arise from use of this software. For more information see the LICENSE file included with this software.
+
+[+] Sending whois query to whois.arin.net
+[+] Response from whois.arin.net:
+
+#
+# ARIN WHOIS data and services are subject to the Terms of Use
+# available at: https://www.arin.net/resources/registry/whois/tou/
+#
+# If you see inaccuracies in the results, please report at
+# https://www.arin.net/resources/registry/whois/inaccuracy_reporting/
+#
+# Copyright 1997-2019, American Registry for Internet Numbers, Ltd.
+#
+
+
+#
+# Query terms are ambiguous.  The query is assumed to be:
+#     "n 140.82.118.3"
+#
+# Use "?" to get help.
+#
+
+NetRange:       140.82.112.0 - 140.82.127.255
+CIDR:           140.82.112.0/20
+NetName:        GITHU
+NetHandle:      NET-140-82-112-0-1
+Parent:         NET140 (NET-140-0-0-0-0)
+NetType:        Direct Assignment
+OriginAS:       AS36459
+Organization:   GitHub, Inc. (GITHU)
+RegDate:        2018-04-25
+Updated:        2019-06-20
+Ref:            https://rdap.arin.net/registry/ip/140.82.112.0
+
+
+OrgName:        GitHub, Inc.
+OrgId:          GITHU
+Address:        88 Colin P Kelly Jr Street
+City:           San Francisco
+StateProv:      CA
+PostalCode:     94107
+Country:        US
+RegDate:        2012-10-22
+Updated:        2014-04-26
+Comment:        https://github.com
+Comment:        Please contact us directly for matters pertaining to abuse.
+Comment:        Urgent matters including DDoS are handled 24x7.
+Ref:            https://rdap.arin.net/registry/entity/GITHU
+
+
+OrgAbuseHandle: GITHU1-ARIN
+OrgAbuseName:   GitHub Abuse
+OrgAbusePhone:  +1-415-857-5430 
+OrgAbuseEmail:  abuse@github.com
+OrgAbuseRef:    https://rdap.arin.net/registry/entity/GITHU1-ARIN
+
+OrgNOCHandle: GITHU-ARIN
+OrgNOCName:   GitHub Ops
+OrgNOCPhone:  +1-415-735-4488 
+OrgNOCEmail:  hostmaster@github.com
+OrgNOCRef:    https://rdap.arin.net/registry/entity/GITHU-ARIN
+
+OrgTechHandle: GITHU-ARIN
+OrgTechName:   GitHub Ops
+OrgTechPhone:  +1-415-735-4488 
+OrgTechEmail:  hostmaster@github.com
+OrgTechRef:    https://rdap.arin.net/registry/entity/GITHU-ARIN
+
+
+#
+# ARIN WHOIS data and services are subject to the Terms of Use
+# available at: https://www.arin.net/resources/registry/whois/tou/
+#
+# If you see inaccuracies in the results, please report at
+# https://www.arin.net/resources/registry/whois/inaccuracy_reporting/
+#
+# Copyright 1997-2019, American Registry for Internet Numbers, Ltd.
+#
+
+
+[+] Report for github.com completed
+```
+
+### Portscanning a Server
+```
+./ipdump.py imap.gmail.com -p -r 900-1000
+[+] WARNING: I am not liable for any damage (including criminal charges) which may arise from use of this software. For more information see the LICENSE file included with this software.
+
+[+] Portscanning imap.gmail.com for open ports in the range 900-1000
+    +-------+------------------------------+-----------+--------------------------------------------------+
+    | Port  | Protocol                     | Transport | Description                                      |
+    +-------+------------------------------+-----------+--------------------------------------------------+
+    | 993   | imaps                        | tcp       | IMAP over TLS protocol                           |
+    | 995   | pop3s                        | tcp       | POP3 over TLS protocol                           |
+    +-------+------------------------------+-----------+--------------------------------------------------+
+[+] Portscan finished
+[+] Report for imap.gmail.com completed
+```
+
+### Importing
+
+`example.py` illustrates how to import IPDump and carry out a simple portscan
+
+```
+# Import the Dumper class
+from ipdump import Dumper
+
+# Create a Dumper with the target "imap.gmail.com"
+dumper = Dumper("imap.gmail.com")
+
+# Print status message
+print("Open Ports: ", end="")
+
+# For each open port, print it to the console
+dumper.get_open_ports(start=1, end=1000, callback=lambda portinfo: print(portinfo.port, end=" "), timeout=1)
+
+# Print a newline, to write PS1 on a newline
+print("")
+```
+
+yielding the following output:
+```
+./example.py 
+Open Ports: 25 465 587 995 993 
+```
---
+++ ipdump.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (c) 2020 fputs
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import argparse
+import requests
+import sys
+import socket
+import ssl
+import threading
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+class Logger:
+	"""
+	Provides formatting for the custom logger
+	"""
+
+	COLOR_DEFAULT  = "\033[0m"
+	COLOR_ERROR    = "\033[91m"
+	COLOR_SUCCESS  = "\033[92m"
+	COLOR_INFO     = "\033[93m"
+
+
+	def __init__(self, enabled=True, color=True):
+		"""
+		Creates a new instance
+		"""
+		self.enabled = enabled
+		self.color = color
+
+	def success(self, msg):
+		"""
+		Logs a success message
+		"""
+		if self.enabled:
+			if self.color:
+				print("{}[+]{} {}".format(self.COLOR_SUCCESS, self.COLOR_DEFAULT, msg))
+			else:
+				print("[*] {}".format(msg))
+
+	def info(self, msg):
+		"""
+		Logs a information message
+		"""
+		if self.enabled:
+			if self.color:
+				print("{}[+]{} {}".format(self.COLOR_INFO, self.COLOR_DEFAULT, msg))
+			else:
+				print("[*] {}".format(msg))
+
+	def error(self, msg):
+		"""
+		Logs a error message
+		"""
+		if self.enabled:
+			if self.color:
+				print("{}[+]{} {}".format(self.COLOR_ERROR, self.COLOR_DEFAULT, msg))
+			else:
+				print("[*] {}".format(msg))
+
+	def no_status(self, msg):
+		"""
+		Logs a message with no status icon
+		"""
+		if self.enabled:
+			print("    " + msg)
+
+class PortInfo:
+	"""
+	Stores information about a specific port
+	"""
+
+	def __init__(self, port, service_name, service_transport, service_desc):
+		self.port = port
+		self.service_name = service_name
+		self.service_transport = service_transport
+		self.service_desc = service_desc
+
+class Dumper:
+	"""
+	Gathers information via APIs and portscanning about a given IP Address, Web Address or Domain
+	"""
+
+	def __init__(self, target):
+		"""
+		Creates a new instance
+		"""
+		self.target = target
+		self.logger = Logger(enabled=False, color=True)
+
+	def attach_logger(self, logger):
+		"""
+		Attaches a logger to the dumper. This directly outputs to stdout
+		"""
+		self.logger = logger		
+
+	def get_ip_info(self):
+		"""
+		Retrieve the information about the IP address from ip-api.com
+		"""
+		base_url = "http://ip-api.com/json/"
+		url_params = "?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,currency,isp,org,as,asname,reverse,mobile,proxy,query"
+		self.logger.info("Requesting information from {}".format(base_url))
+		response = requests.get(base_url + str(self.target) + url_params)
+		if response.status_code != 200:
+			self.logger.error("Unable to connect to {} (Code {})".format(base_url, response.status_code))
+		else:
+			response_json = response.json()
+			if response_json["status"] == "success":
+				self.logger.success("Response from {}:".format(base_url))
+				return response.json()
+			else:
+				self.logger.error("Unable to fetch information from {} (Reason: {})".format(base_url, response_json["message"]))
+		return dict()
+
+	def get_ssl_info(self, timeout=5):
+		"""
+		Retrieve the SSL certificate from the host
+		"""
+		ctx = ssl.create_default_context()
+		s = ctx.wrap_socket(socket.socket(), server_hostname=str(self.target))
+		s.settimeout(timeout)
+		try:
+			s.connect((str(self.target), 443))
+		except Exception as e:
+			self.logger.error("Unable to connect to {} (Reason: {})".format(str(self.target), e))
+			return dict()
+
+		cert = s.getpeercert()
+		s.close()
+		self.logger.success("Certificate: ")
+		return dict(cert)
+
+	def get_whois_info(self, timeout=5):
+		"""
+		Retrieve the whois information for the targetfrom whois.arin.net
+		"""
+		base_url = "whois.arin.net"
+		self.logger.info("Sending whois query to {}".format(base_url))
+		s = None
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((base_url, 43))
+		except Exception as e:
+			self.logger.error("Unable to connect to {} (Reason: {})".format(str(self.target), e))
+			s.close()
+			return ""
+		
+		host_address = ""
+		try:
+			host_address = socket.gethostbyname(self.target)
+		except Exception as e:
+			self.logger.error("Unable to connect to {} (Reason: {})".format(str(self.target), e))
+			s.close()
+			return ""
+
+		s.send((host_address + "\r\n").encode())
+		response = b""
+		while True:
+			data = s.recv(4096)
+			response += data
+			if not data:
+				break
+
+		s.close()
+		self.logger.success("Response from {}:".format(base_url))
+		return response.decode()
+
+	def __check_port(self, port_no, callback, timeout=5):
+		"""
+		Tests if the given port is open on the target, if it is, the callback function is executed with one argument of type PortInfo
+		"""
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.settimeout(timeout)
+		try:
+			con = s.connect((self.target, port_no))
+			try:
+				service_info = find_service(port_no)
+				port = str(port_no)
+				service_name = service_info[0]
+				service_transport = service_info[2]
+				service_desc = service_info[3]
+				callback(PortInfo(port, service_name, service_transport, service_desc))
+			except Exception as e:
+				self.logger.error("Unable to scan port {} (Reason: {})".format(str(port_no), e))
+			con.close()
+		except Exception as e:
+			pass
+
+	def get_open_ports(self, callback, workers=100, start=1, end=1000, timeout=5):
+		"""
+		Gets the open ports running on the target and prints them as a table.
+		"""
+		self.logger.info("Portscanning {} for open ports in the range {}-{}".format(self.target, start, end))
+		self.logger.no_status("+-------+------------------------------+-----------+{}+".format("-" * 50))
+		self.logger.no_status("| {} | {} | {} | {} |".format("Port".ljust(5), "Protocol".ljust(28), "Transport".ljust(9), "Description".ljust(48)))
+		self.logger.no_status("+-------+------------------------------+-----------+{}+".format("-" * 50))
+		with ThreadPoolExecutor(max_workers=workers) as executor:
+			for port in range(start, end+1):
+				executor.submit(self.__check_port, port, callback, timeout)
+
+		self.logger.no_status("+-------+------------------------------+-----------+{}+" .format("-" * 50))
+		self.logger.success("Portscan finished")
+
+
+def print_dict(d):
+	"""
+	Prints the given dictionary in key-value pairs
+	"""
+	for k, v in d.items():
+		print("{}: {}".format(k.ljust(20), v))
+
+
+def find_service(port_no):
+	"""
+	Retrieves information about the service running on the given port.
+	This information is read from services.csv
+	"""
+
+	if os.path.isfile("services.csv"):
+		f = open("services.csv")
+		line = f.readline()
+		while line != '':
+			if line.count(",") < 11:
+				line += f.readline()
+			else:
+				if line.split(",")[1] == str(port_no):
+					f.close()
+					return line.split(",")
+				line = f.readline()
+		f.close()
+
+	return ["Unknown"] * 12
+
+
+def print_port_info(portinfo: PortInfo):
+	"""
+	Prints the information of a port formatted to match the table in Dumper.get_open_ports
+	"""
+	port = str(portinfo.port).ljust(5)
+	service_name = (portinfo.service_name[:25] + "..." if len(portinfo.service_name) >= 28 else portinfo.service_name).ljust(28)
+	service_transport = portinfo.service_transport.ljust(9)
+	service_desc = (portinfo.service_desc[:45] + "..." if len(portinfo.service_desc) >= 48 else portinfo.service_desc).ljust(48)
+	print("    | {} | {} | {} | {} |".format(port, service_name, service_transport, service_desc))
+
+if __name__ == "__main__":
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument("host", help="The hostname/IP Address, URL or Domain of the target", type=str)
+	parser.add_argument("-l", "--no-logging", help="Disable logging", action="count")
+	parser.add_argument("-c", "--no-color", help="Disable colored logging", action="count")
+	parser.add_argument("-a", "--all", help="Run all tools on the given target", action="count")
+	parser.add_argument("-p", "--port-scan", help="Enable portscanning on the target", action="count")
+	parser.add_argument("-i", "--ip-info", help="Fetch information from api-ip.com (contains geographical info)", action="count")
+	parser.add_argument("-s", "--ssl-cert", help="Retrieves the SSL Certificate of the host", action="count")
+	parser.add_argument("-w", "--whois", help="Fetch whois information from arin.net (contains domain ownership info)", action="count")
+	parser.add_argument("-n", "--workers", help="Number of workers for portscanning", type=int, default=256)
+	parser.add_argument("-r", "--range", help="Range of ports to scan formatted as START-END", type=str, default="1-1024")
+	parser.add_argument("-t", "--timeout", help="Timeout for SSL and WHOIS fetching and portscanning", type=int, default=5)
+	args = parser.parse_args()
+	
+	logger = Logger(enabled=args.no_logging == None, color=args.no_color == None)
+	dumper = Dumper(args.host)
+	
+	dumper.attach_logger(logger)
+
+	logger.info("WARNING: I am not liable for any damage (including criminal charges) which may arise from use of this software." \
+		" For more information see the LICENSE file included with this software.\n")
+
+	if args.all != None or args.ip_info != None:
+		print_dict(dumper.get_ip_info())
+	if args.all != None or args.ssl_cert != None:
+		print_dict(dumper.get_ssl_info(timeout=args.timeout))
+	if args.all != None or args.whois != None:
+		print(dumper.get_whois_info(timeout=args.timeout))
+	if args.all != None or args.port_scan != None:
+		dumper.get_open_ports(workers=args.workers, 
+			start=int(args.range.split("-")[0]), 
+			end=int(args.range.split("-")[1]), 
+			callback=print_port_info, 
+			timeout=args.timeout)
+		
+	logger.info("Report for {} completed".format(args.host))
