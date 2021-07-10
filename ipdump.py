b0ebbd4ebe0ec61923d41029b3cccefaee1db25b
Adam Bruce <adam@adambruce.net>
2021-07-10 17:01:39 +0100

    Improved comments and docs

--- ipdump.py
+++ ipdump.py
@@ -1,27 +1,5 @@
 #!/usr/bin/env python3
 
-# MIT License
-#
-# Copyright (c) 2020 fputs
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import argparse
 import requests
 import sys
@@ -33,65 +11,78 @@ from concurrent.futures import ThreadPoolExecutor
 
 class Logger:
 	"""
-	Provides formatting for the custom logger
+	Provides formatting for the custom logger.
 	"""
 
-	COLOR_DEFAULT  = "\033[0m"
-	COLOR_ERROR    = "\033[91m"
-	COLOR_SUCCESS  = "\033[92m"
-	COLOR_INFO     = "\033[93m"
-
+	COLOR_DEFAULT  = "\033[0m"   # White
+	COLOR_ERROR    = "\033[91m"  # Red
+	COLOR_SUCCESS  = "\033[92m"  # Green
+	COLOR_INFO     = "\033[93m"  # Orange
 
 	def __init__(self, enabled=True, color=True):
 		"""
-		Creates a new instance
+		Creates a new logger instance.
+
+		:param enabled: if true, logging is enabled.
+		:param color: if true, the logger will output colored text.
 		"""
 		self.enabled = enabled
 		self.color = color
 
-	def success(self, msg):
-		"""
-		Logs a success message
-		"""
+	def log_status(self, msg, color=COLOR_DEFAULT):
 		if self.enabled:
 			if self.color:
-				print("{}[+]{} {}".format(self.COLOR_SUCCESS, self.COLOR_DEFAULT, msg))
+				print("{}[+]{} {}".format(color, self.COLOR_DEFAULT, msg))
 			else:
 				print("[*] {}".format(msg))
+
+	def success(self, msg):
+		"""
+		Logs a success message.
+
+		:param msg: the message to log.
+		"""
+		self.log_status(msg, self.COLOR_SUCCESS)
+
 
 	def info(self, msg):
 		"""
-		Logs a information message
+		Logs a information message.
+
+		:param msg: the message to log.
 		"""
-		if self.enabled:
-			if self.color:
-				print("{}[+]{} {}".format(self.COLOR_INFO, self.COLOR_DEFAULT, msg))
-			else:
-				print("[*] {}".format(msg))
+		self.log_status(msg, self.COLOR_INFO)
 
 	def error(self, msg):
 		"""
-		Logs a error message
+		Logs a error message.
 		"""
-		if self.enabled:
-			if self.color:
-				print("{}[+]{} {}".format(self.COLOR_ERROR, self.COLOR_DEFAULT, msg))
-			else:
-				print("[*] {}".format(msg))
+		self.log_status(msg, self.COLOR_ERROR)
 
 	def no_status(self, msg):
 		"""
-		Logs a message with no status icon
+		Logs a message with no status icon.
+
+		:param msg: the message to log.
 		"""
 		if self.enabled:
 			print("    " + msg)
 
 class PortInfo:
 	"""
-	Stores information about a specific port
+	Stores information about a specific port.
 	"""
 
 	def __init__(self, port, service_name, service_transport, service_desc):
+		"""
+		Creates a new PortInfo instance.
+
+		:param port: The port number.
+		:param service_name: The name of the service (protocol) running on the port.
+		:param service_transport: The transport layer protocol operating on the port.
+		:param service_desc: A description of the protocol.
+		"""
+
 		self.port = port
 		self.service_name = service_name
 		self.service_transport = service_transport
@@ -99,25 +90,30 @@ class PortInfo:
 
 class Dumper:
 	"""
-	Gathers information via APIs and portscanning about a given IP Address, Web Address or Domain
+	Gathers information via APIs and portscanning about a given IP Address, Web Address or Domain.
 	"""
 
 	def __init__(self, target):
 		"""
-		Creates a new instance
+		Creates a new Dumper instance.
+
+		:param target: the target host.
 		"""
+
 		self.target = target
 		self.logger = Logger(enabled=False, color=True)
 
 	def attach_logger(self, logger):
 		"""
-		Attaches a logger to the dumper. This directly outputs to stdout
+		Attaches a logger to the dumper. This directly outputs to stdout.
+
+		:param logger: the logger to attach to the dumper.
 		"""
 		self.logger = logger		
 
 	def get_ip_info(self):
 		"""
-		Retrieve the information about the IP address from ip-api.com
+		Retrieve the information about the IP address from ip-api.com.
 		"""
 		base_url = "http://ip-api.com/json/"
 		url_params = "?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,currency,isp,org,as,asname,reverse,mobile,proxy,query"
@@ -136,8 +132,11 @@ class Dumper:
 
 	def get_ssl_info(self, timeout=5):
 		"""
-		Retrieve the SSL certificate from the host
+		Retrieve the SSL certificate from the host.
+
+		:param timeout: the timeout for the SSL connection.
 		"""
+
 		ctx = ssl.create_default_context()
 		s = ctx.wrap_socket(socket.socket(), server_hostname=str(self.target))
 		s.settimeout(timeout)
@@ -154,8 +153,11 @@ class Dumper:
 
 	def get_whois_info(self, timeout=5):
 		"""
-		Retrieve the whois information for the targetfrom whois.arin.net
+		Retrieve the whois information for the targetfrom whois.arin.net.
+
+		:param timout: the timeout for the WHOIS request.
 		"""
+
 		base_url = "whois.arin.net"
 		self.logger.info("Sending whois query to {}".format(base_url))
 		s = None
@@ -189,8 +191,13 @@ class Dumper:
 
 	def __check_port(self, port_no, callback, timeout=5):
 		"""
-		Tests if the given port is open on the target, if it is, the callback function is executed with one argument of type PortInfo
+		Tests if the given port is open on the target, if it is, the callback function is executed with one argument of type PortInfo.
+		
+		:param port_no: the port number to check.
+		:param callback: the function to call upon completion.
+		:param timeout: the time to wait for a response.
 		"""
+
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.settimeout(timeout)
 		try:
@@ -211,7 +218,14 @@ class Dumper:
 	def get_open_ports(self, callback, workers=100, start=1, end=1000, timeout=5):
 		"""
 		Gets the open ports running on the target and prints them as a table.
+
+		:param callback: the function to call upon completion.
+		:param workers: the number of workers (threads) to use.
+		:param start: the start port number.
+		:param end: the end port number.
+		:param timeout: the time to wait for a response.
 		"""
+
 		self.logger.info("Portscanning {} for open ports in the range {}-{}".format(self.target, start, end))
 		self.logger.no_status("+-------+------------------------------+-----------+{}+".format("-" * 50))
 		self.logger.no_status("| {} | {} | {} | {} |".format("Port".ljust(5), "Protocol".ljust(28), "Transport".ljust(9), "Description".ljust(48)))
@@ -226,8 +240,11 @@ class Dumper:
 
 def print_dict(d):
 	"""
-	Prints the given dictionary in key-value pairs
+	Prints the given dictionary in key-value pairs.
+
+	:param d: the dictionary to print
 	"""
+
 	for k, v in d.items():
 		print("{}: {}".format(k.ljust(20), v))
 
@@ -235,7 +252,9 @@ def print_dict(d):
 def find_service(port_no):
 	"""
 	Retrieves information about the service running on the given port.
-	This information is read from services.csv
+	This information is read from services.csv.
+
+	:param port_no: the port number to find the service / protocol for.
 	"""
 
 	if os.path.isfile("services.csv"):
@@ -254,9 +273,11 @@ def find_service(port_no):
 	return ["Unknown"] * 12
 
 
-def print_port_info(portinfo: PortInfo):
+def print_port_info(portinfo):
 	"""
-	Prints the information of a port formatted to match the table in Dumper.get_open_ports
+	Prints the information of a port formatted to match the table in Dumper.get_open_ports.
+	
+	:param portinfo: a PortInfo object to display as a table.
 	"""
 	port = str(portinfo.port).ljust(5)
 	service_name = (portinfo.service_name[:25] + "..." if len(portinfo.service_name) >= 28 else portinfo.service_name).ljust(28)
